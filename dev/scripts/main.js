9cbc8ed4ea4b4c940cf1c574ffad9d6b45f75c2d
Kimberly Luu <kimzillacode@gmail.com>
2017-06-20 11:43:54 -0400

    organized partials, made responsive, added working links

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,4 +1,49 @@
-const ohHey = "Hello World";
+console.log("Hey there!")
+console.log("Welcome to my page (: ")
 
-console.log(ohHey);
-console.log("ROOTBEER")
+$(document).ready(function(){
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').animate({
+			'scrollTop': $target.offset().top
+		}, 750, 'swing');
+	});
+});
+/*$('.burger').click(function () {
+  $(this).toggleClass('open');
+});*/
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
